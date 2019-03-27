23a4e1cafc12d5c361ad303fc2e57c8ccf6926fa
DixonTheCreator <dixonthecreator@gmail.com>
2019-03-27 23:28:47 +0300

    Beginning of the project Done: BottomNavigationView logic with 3 fragments, added TopAppBar

---
+++ app/src/main/java/com/wordpress/dixonthecreator/studentrecipebook/FavsFragment.java
@@ -0,0 +1,20 @@
+package com.wordpress.dixonthecreator.studentrecipebook;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FavsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.favs_fragment, container, false);
+
+    //TODO: FavsFragment logic: adding recipes due to user's desire
+    }
+}
---
+++ app/src/main/java/com/wordpress/dixonthecreator/studentrecipebook/MainActivity.java
@@ -0,0 +1,49 @@
+package com.wordpress.dixonthecreator.studentrecipebook;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //OnCreate Fragment transaction to Home fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+        BottomNavigationTransaction();
+    }
+
+    //Bottom Navigation transactions logic
+    private void BottomNavigationTransaction() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment selectedFragment = null;
+                switch (menuItem.getItemId()) {
+                    case R.id.bn_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.bn_favs:
+                        selectedFragment = new FavsFragment();
+                        break;
+                    case R.id.bn_search:
+                        selectedFragment = new SearchFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                return true;
+            }
+        });
+    }
+
+}
